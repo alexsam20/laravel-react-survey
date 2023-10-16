c2800b8e7157b562b6c175d2ae94159098f418a5
Alex Sam <alsam3637@gmail.com>
2023-10-15 22:56:35 -0400

    Create TButton component Edit Survey page.

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -198,7 +198,7 @@ export const ContextProvider = ({children}) => {
     >
       {children}
     </StateContext.Provider>
-  )
+  );
 };
 
 export const useStateContext = () => useContext(StateContext);
--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,16 +1,20 @@
 import PageComponent from "../components/PageComponent.jsx";
-import {useStateContext} from "../contexts/ContextProvider.jsx";
 import SurveyListItem from "../components/SurveyListItem.jsx";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 export default function Surveys() {
   const {surveys} = useStateContext();
   console.log(surveys);
 
+  const onDeleteClick = () => {
+      console.log("on Delete click");
+  }
+
   return (
     <PageComponent title="Surveys">
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
         {surveys.map((survey) => (
-            <SurveyListItem survey={survey} key={survey.id} />
+            <SurveyListItem survey={survey} key={survey.id} onDeleteClick={onDeleteClick} />
         ))}
       </div>
     </PageComponent>
